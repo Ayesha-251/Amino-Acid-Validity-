1f1660097a8c094a26445c24f8733414ada84462
Ayesha Ansar <ayeshaansar678@gmail.com>
2026-07-12 19:11:17 +0500

    Update Amino_acid_Validity.py

--- Amino_acid_Validity.py
+++ Amino_acid_Validity.py
@@ -1,9 +1,14 @@
-#FOR DETERMINING ONLY THE INVALID AMINO ACID IN THE PROTEIN SEQUENCE     
+#FOR DETERMINING THE INVALID AMINO ACID IN THE PROTEIN SEQUENCE AND UPDATING THE SEQUENCE BY REMOVING THE INVALID SEQUENCES 
 protein=input("Enter a Protein Sequence: ").upper().strip()
+corrected_sequence=""
 Invalid_found = False 
 for i in range(len(protein)):
     if protein[i] not in "ACDEFGHIKLMNPQRSTVWY":
         print(f"Invalid amino acid {protein[i]} at position {i}")
         Invalid_found=True 
+        continue
+    corrected_sequence=corrected_sequence+(protein[i])
 if not Invalid_found:
     print("The Protein sequence is Valid")
+else:
+    print(f"Corrected sequence is {corrected_sequence}")
